3fcd97d285b775dc7752f73c4db0ff091dd4c4eb
deng <2683999020@qq.com>
2019-12-14 21:28:56 +0800

    ``

--- src/page/zixun/zixun.js
+++ src/page/zixun/zixun.js
@@ -2,20 +2,38 @@ import { log } from "util";
 export default {
   data() {
     return {
-      message: "",
-      mes: "",
-      xx: "<br/>"
+      message: {},
+      mes:[],
+      
+    };
+  },
+  created: function() {
+    var lett = this;
+    //初始化实例的时候给DOM绑定键盘事件
+    document.onkeydown = function(event) {
+      if (event.keyCode == 13 && event.ctrlKey) {
+        lett.send(event);
+      }
     };
   },
   methods: {
+    //监听input事件
     search: function(event) {
-      this.message += event.currentTarget.value
-      console.log(this.message);
+      this.message = event.target.value
+      console.log(this.message)
       
     },
-    send: function() {
-   
+    
+    send: function(e) {
       
+      if (this.message == "") {
+        alert("请输入内容");
+      } else {
+        
+        this.mes.push(this.message)
+        e.target.value=''
+        
+      }
     }
   }
 };
--- src/page/zixun/zixun.vue
+++ src/page/zixun/zixun.vue
@@ -1,29 +1,25 @@
 <template>
-  <div id="zixun">
-    <header class="tou">
-      <img src="../../common/img/咨询/icon_left - 副本.png" />
-      <span>在线咨询</span>
-      <img src="../../common/img/咨询/ling.png" />
-    </header>
-    <div class="cont" >{{ mes}}{{message}}</div>
-    <div class="dialogude">
-      <div >
-          <!-- @input @keyup.enter  document.getbyId().value 都可以拿到inpu 值 -->
-          <input
-            placeholder="请输入要咨询的信息"
-            autofocus="autofocus"
-            @keyup.enter= 'search'
-          />
-
-          <!-- <img src="../../common/img/咨询/biaoqing.png" /> -->
-        <button class="fa" type = 'submit' @click="send">发送</button>
-      </div>
-    </div>
+<div class="zixun">
+  <header></header>
+  <div class="cont">
+    {{mes}}
   </div>
+    <form action="" class="from">
+      <textarea name='' @input='search'>
+
+      </textarea>
+      <div>
+        <img src="../../common/img/咨询/biaoqing.png" alt="">
+      </div>
+      <input type="submit" class="but" value="发送" 
+        @click="send"
+      />
+    </form>
+</div>
+
+</form>
 </template>
-<script src= './zixun.js'>
-  
-</script>
+<script src="./zixun.js"></script>
 <style lang="less" scoped>
 @import url("./less/zixun.less");
 </style>
